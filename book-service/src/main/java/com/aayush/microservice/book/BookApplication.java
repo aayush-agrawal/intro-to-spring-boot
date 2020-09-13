4459887e1276d3ce869883eb9fa0997b6a08713b
Aayush Agrawal <Aayush_Agrawal@amat.com>
2020-09-13 14:10:21 +0530

    added dockerfile and travis file

--- book-service/src/main/java/com/aayush/microservice/book/BookApplication.java
+++ book-service/src/main/java/com/aayush/microservice/book/BookApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MicroserviceExampleApplication {
+public class BookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceExampleApplication.class, args);
+		SpringApplication.run(BookApplication.class, args);
 	}
 
 }
